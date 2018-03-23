25df97832a02aacdf06276fbf5c84db1c952a051
Sergey Kornitsky <skornitsky@gmail.com>
2018-03-23 18:08:07 +0200

    Implemented search form submitting

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,4 +1,163 @@
-function test(){
-    alert("Loaded");
+if(!!window.React ||
+   !!document.querySelector('[data-reactroot], [data-reactid]'))
+  console.log('React.js');
+
+if(!!window.angular ||
+   !!document.querySelector('.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]') ||
+   !!document.querySelector('script[src*="angular.js"], script[src*="angular.min.js"]'))
+  console.log('Angular.js');
+
+if(!!window.Backbone) {console.log('Backbone.js') };
+if(!!window.Ember) console.log('Ember.js');
+if(!!window.Vue) console.log('Vue.js');
+if(!!window.Meteor) console.log('Meteor.js');
+if(!!window.Zepto) console.log('Zepto.js');
+if(!!window.jQuery) console.log('jQuery.js');
+
+var getRandDelay = function(){
+ return Math.floor(500 + (Math.random() * 1000) );
 }
 
+function showAndroidToast(toast) { Android.showToast(toast); };
+
+var triggerReactEvent = function(input, value){
+    let lastValue = input.value;
+    input.value = value;
+    let event = new Event('input', {'text': value });
+    // hack React15
+    event.simulated = true;
+    // hack React16
+    let tracker = input._valueTracker;
+    if (tracker) {
+      tracker.setValue(lastValue);
+    }
+    input.dispatchEvent(event);
+}
+
+var getElementsByXPath = function(xpath, startNode, resultType) {
+    startNode  = (typeof startNode === 'undefined') ? document : startNode;
+    resultType = (typeof resultType === 'undefined') ? XPathResult.ANY_TYPE : resultType;
+
+    return document.evaluate(xpath, startNode, null, resultType, null);
+}
+
+var getElementByXPath = function(xpath, startNode) {
+    return getElementsByXPath(xpath, startNode, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
+}
+
+
+//skyscanner functions
+//day-list-total
+
+var timer = setInterval(function() {
+    let element = getElementByXPath("//div[contains(@class,'day-list-total')]");
+    let d = (element != null)? element.textContent : "";
+    showAndroidToast("day-list-total:" + d );
+
+    if (false) clearInterval(timer);
+  }, 2000);
+
+function searchFormSubmission(){
+
+    showAndroidToast("search form start");
+
+    //origin input
+    triggerReactEvent(document.getElementById('js-origin-input'), 'ATL');
+
+    showAndroidToast("origin airport selected");
+
+
+    setTimeout(function() {
+
+        //select first element in origin dropdown
+        getElementByXPath('(//div[contains(@class,"-dataset-origin")]//*[contains(@class,"airport")])[1]').click();
+        showAndroidToast("origin airport selected in dropdown");
+
+        setTimeout(function() {
+
+            //destination
+            triggerReactEvent(document.getElementById('js-destination-input'), 'MSK');
+            showAndroidToast("typed MSK");
+            setTimeout(function() {
+                //click on first element in destination dropdown
+                getElementByXPath('(//div[contains(@class,"-dataset-destination")]//*[contains(@class,"airport")])[1]').click();
+                showAndroidToast("click on first destination in the list");
+                setTimeout(function() {
+                    //click on submit button
+                    getElementByXPath('//button[contains(@class,"fss-bpk-button--large")]').click();
+                    showAndroidToast("form submitted");
+
+                }, getRandDelay());
+            }, getRandDelay());
+        }, getRandDelay());
+
+    }, getRandDelay());
+
+
+
+    return document.title;
+}
+
+var getPricesFromSkyScrapper = function(){
+
+    let elements = getElementsByXPath('//li[contains(@class,"day-list-item")]');
+
+    let currentElement = elements.iterateNext();
+
+    let alertText = "Found Prices:\n";
+    while (currentElement) {
+      console.log(currentElement);
+      let airlinesElement = getElementByXPath('.//span[contains(@class, "airline-text") or contains(@class,"airline-name")]/text()', currentElement);
+      let priceElement = getElementByXPath('.//a[contains(@class, "price")]/text()', currentElement);
+
+        let airline = (airlinesElement != null) ? airlinesElement.textContent : "";
+        let priceC = (priceElement != null) ? priceElement.textContent: "";
+
+      alertText += airline + " - " + priceC + "\n";
+
+      currentElement = elements.iterateNext();
+    }
+    return alertText;
+}
+
+
+function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+
+
+function tapOnElement( el ) {
+    var type = 'move'; // or start, end
+    var event = document.createEvent('Event');
+    event.initEvent('touch' + type, true, true);
+    event.constructor.name; // Event (not TouchEvent)
+
+    var offsetElement = getOffset(el);
+    var x=offsetElement.top, y=offsetElement.left;
+
+    var point = {x: x, y: y };
+    event.touches = [{
+        identifier: Date.now() + i,
+        pageX: x,
+        pageY: y,
+        screenX: x,
+        screenY: y,
+        clientX: x,
+        clientY: y
+    }, {  identifier: Date.now() + i,
+        pageX: point.x,
+        pageY: point.y,
+        screenX: point.x,
+        screenY: point.y,
+        clientX: point.x,
+        clientY: point.y}]
+
+        dispatchEvent(event);
+}
--- app/src/main/java/tech/mangosoft/flightscan/SearchKayak.java
+++ app/src/main/java/tech/mangosoft/flightscan/SearchKayak.java
@@ -1,18 +1,26 @@
 package tech.mangosoft.flightscan;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
 import android.webkit.ValueCallback;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,11 +40,14 @@ public class SearchKayak extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
         myWebView.setWebViewClient(new WebViewClient() {
 
             @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest url){
-                if (url.getUrl().toString().indexOf("px/client")>0) {
+                String urlString = url.getUrl().toString();
+                if (!(!urlString.contains("px/client") && !urlString.contains("mixpanel555"))) {
                     resources.add(url.getUrl().toString());
                     try {
                         InputStream is = getAssets().open("script.js");
@@ -57,15 +68,17 @@ public class SearchKayak extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
 
-                //injectScriptFile(view, "script.js"); // see below ...
+                injectScriptFile(view, "script.js"); // see below ...
 
                 // test if the script was loaded
-               view.evaluateJavascript("javascript:setTimeout(test(), 500)", new ValueCallback<String>() {
+/*               view.evaluateJavascript("(function() { return searchFormSubmission(); })();", new ValueCallback<String>() {
                    @Override
                    public void onReceiveValue(String s) {
                        System.out.println(s);
                    }
                });
+               */
+
             }
 
             private void injectScriptFile(WebView view, String scriptFile) {
@@ -78,14 +91,20 @@ public class SearchKayak extends AppCompatActivity {
 
                     // String-ify the script byte-array using BASE64 encoding !!!
                     String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
-                    view.loadUrl("javascript:(function() {" +
+                    view.evaluateJavascript("javascript:(function() {" +
                             "var parent = document.getElementsByTagName('head').item(0);" +
                             "var script = document.createElement('script');" +
                             "script.type = 'text/javascript';" +
                             // Tell the browser to BASE64-decode the string into your script !!!
                             "script.innerHTML = window.atob('" + encoded + "');" +
-                            "parent.appendChild(script)" +
-                            "})()");
+                            "let as = parent.appendChild(script);" +
+//                            "return  setTimeout(function() {searchFormSubmission(); }, 500); "+
+                            "})()", new ValueCallback<String>(){
+                            @Override
+                            public void onReceiveValue(String s) {
+                                System.out.println(s);
+                            }
+                    });
                 } catch (IOException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -93,10 +112,47 @@ public class SearchKayak extends AppCompatActivity {
             }
         });
 
-        myWebView.loadUrl("*");
+       // myWebView.loadUrl("http://www.kayak.com");
+        myWebView.loadUrl("https://www.skyscanner.net");
+
     }
 
     protected void onFinishInflate() {
 
     }
+
+    public String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e("error", ex.toString());
+        }
+        return null;
+    }
+
+
+
+    protected class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
